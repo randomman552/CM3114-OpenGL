cd3fef9b4227d6bd5dc9730977acbba674ba5915
George Grainger <random.man552@protonmail.com>
2021-12-06 19:02:12 +0000

    Add texture coordinates to cube and correct normals

--- src/Objects/SCube.java
+++ src/Objects/SCube.java
@@ -15,48 +15,155 @@ public class SCube extends SObject {
 
     @Override
     protected void genData() {
+        // Quite a bit of redundant data here, but simplifying would likely increase complexity
         // How far each vert is from the center of the cube
         float size = this.size / 2;
 
-        // Generate vertices
+        // region Generate vertices
+        // Use 24 vertices, so we can have proper normals on each face
         vertices = new float[] {
+            // Back
             -size, -size, -size,
              size, -size, -size,
             -size,  size, -size,
              size,  size, -size,
-            -size, -size,  size,
-             size, -size,  size,
+
+            // Top
+            -size,  size, -size,
+             size,  size, -size,
             -size,  size,  size,
-             size,  size,  size
+             size,  size,  size,
+
+            // Bottom
+            -size,  -size, -size,
+             size,  -size, -size,
+            -size,  -size,  size,
+             size,  -size,  size,
+
+            // Front
+            -size, -size, size,
+             size, -size, size,
+            -size,  size, size,
+             size,  size, size,
+
+            // Left
+            -size, -size, -size,
+            -size,  size, -size,
+            -size, -size,  size,
+            -size,  size,  size,
+
+            // Right
+            size, -size, -size,
+            size,  size, -size,
+            size, -size,  size,
+            size,  size,  size,
         };
-        numVertices = vertices.length;
+        numVertices = vertices.length / 3;
+        // endregion
 
-        // Generate normals
-        normals = vertices;
+        // region Generate normals
+        normals = new float[] {
+                // Back
+                0, 0, -1,
+                0, 0, -1,
+                0, 0, -1,
+                0, 0, -1,
 
-        // Generate textures
-        textures = new float[numVertices * 3];
+                // Top
+                0, 1, 0,
+                0, 1, 0,
+                0, 1, 0,
+                0, 1, 0,
 
+                // Bottom
+                0, -1, 0,
+                0, -1, 0,
+                0, -1, 0,
+                0, -1, 0,
+
+                // Front
+                0, 0, 1,
+                0, 0, 1,
+                0, 0, 1,
+                0, 0, 1,
+
+                // Left
+                -1, 0, 0,
+                -1, 0, 0,
+                -1, 0, 0,
+                -1, 0, 0,
+
+                // Right
+                1, 0, 0,
+                1, 0, 0,
+                1, 0, 0,
+                1, 0, 0,
+        };
+        // endregion
+
+        // region Generate textures
+        textures = new float[] {
+                // Back
+                1, 0,
+                0, 0,
+                1, 1,
+                0, 1,
+
+                // Top
+                0, 1,
+                1, 1,
+                0, 0,
+                1, 0,
+
+                // Bottom
+                0, 1,
+                1, 1,
+                0, 0,
+                1, 0,
+
+                // Front
+                0, 0,
+                1, 0,
+                0, 1,
+                1, 1,
+
+                // Left
+                0, 0,
+                0, 1,
+                1, 0,
+                1, 1,
+
+                // Right
+                1, 0,
+                1, 1,
+                0, 0,
+                0, 1,
+        };
+
+        // endregion
+
+        // region Generate indices
         indices = new int[] {
-            2, 1, 0, // Back 1
-            1, 2, 3, // Back 2
+                2, 1, 0, // Back 1
+                1, 2, 3, // Back 2
 
-            6, 7, 2, // Top 1
-            3, 2, 7, // Top 2
+                6, 5, 4, // Top 1
+                5, 6, 7, // Top 2
 
-            0, 1, 4, // Bottom 1
-            5, 4, 1, // Bottom 2
+                8, 9, 10, // Bottom 1
+                11, 10, 9, // Bottom 2
 
-            4, 5, 6, // Front 1
-            7, 6, 5, // Front 2
+                12, 13, 14, // Front 1
+                15, 14, 13, // Front 2
 
-            5, 1, 7, // Right 1
-            3, 7, 1, // Right 2
+                18, 17, 16, // Left 1
+                17, 18, 19, // Left 2
 
-            0, 4, 2, // Left 1
-            6, 2, 4, // Left 2
+                20, 21, 22, // Right 1
+                23, 22, 21, // Right 2
         };
         numIndices = indices.length;
+        // endregion
     }
 
     public void setSize(float size) {
